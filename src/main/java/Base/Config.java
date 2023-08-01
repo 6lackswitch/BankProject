5c0c083a922e41577284bd46025fa6c3a0567ba4
6lackswitch <6lackswitch@gmail.com>
2023-08-01 22:41:57 +0300

    some fixes

---
+++ src/main/java/Base/Config.java
@@ -0,0 +1,8 @@
+package Base;
+
+public class Config {
+    public static final String URL = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login";
+    public static final String HUB = "http://localhost:4444/wd/hub";
+    public static final String PATH = "src/main/resources/file.csv";
+    public static final String DRIVER_PATH = "src\\main\\resources\\chromedriver.exe";
+}
